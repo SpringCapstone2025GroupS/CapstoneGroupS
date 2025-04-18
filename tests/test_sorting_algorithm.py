c8cddda119c147d6a1c93d75e7cb68f11ba1c0ee
Jacob Dearborn <jtdear4@ou.edu>
2025-04-18 12:40:52 -0500

    [CAP-42] Moved functions back out

--- tests/test_sorting_algorithm.py
+++ tests/test_sorting_algorithm.py
@@ -1,7 +1,7 @@
 import pytest
 from datetime import datetime, timedelta, UTC
 from notam_fetcher.api_schema import Notam, PurposeType, NotamType, Classification
-from sorting_algorithm.sorting_algorithm import NotamSorter
+from sorting_algorithm.sorting_algorithm import NotamSorter, score_by_purpose, score
 
 @pytest.fixture
 def sample_notam_1():
@@ -61,16 +61,15 @@ def sample_notam_3():
     )
 
 def test_score_by_purpose(sample_notam_1: Notam, sample_notam_2: Notam, sample_notam_3: Notam):
-    sorter = NotamSorter([sample_notam_1, sample_notam_2, sample_notam_3])
-    assert sorter.score_by_purpose(sample_notam_1) == 50  # PurposeType.N
-    assert sorter.score_by_purpose(sample_notam_2) == 25  # PurposeType.B
-    assert sorter.score_by_purpose(sample_notam_3) == 10  # PurposeType.O
+    assert score_by_purpose(sample_notam_1) == 50  # PurposeType.N
+    assert score_by_purpose(sample_notam_2) == 25  # PurposeType.B
+    assert score_by_purpose(sample_notam_3) == 10  # PurposeType.O
 
 def test_score(sample_notam_1: Notam, sample_notam_2: Notam, sample_notam_3: Notam):
     sorter = NotamSorter([sample_notam_1, sample_notam_2, sample_notam_3])
-    assert sorter.score(sample_notam_1) == 50  # Only score_by_purpose is used
-    assert sorter.score(sample_notam_2) == 25
-    assert sorter.score(sample_notam_3) == 10
+    assert score(sample_notam_1) == 50  # Only score_by_purpose is used
+    assert score(sample_notam_2) == 25
+    assert score(sample_notam_3) == 10
 
 def test_sort_by_score(sample_notam_1: Notam, sample_notam_2: Notam, sample_notam_3: Notam):
     notams = [sample_notam_3, sample_notam_1, sample_notam_2]
